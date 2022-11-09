0fb8f9cc2208d137dfc1303c13051ea57c5a03a8
Mao Li <mli94@stevens.edu>
2022-11-09 17:54:43 -0500

    added Lab 8

---
+++ Lab8_Stub/routes/people.js
@@ -0,0 +1,13 @@
+//Require express and express router as shown in lecture code and worked in previous labs
+
+router.route("/").get(async (req, res) => {
+  //code here for GET
+});
+
+router.route("/searchpeople").post(async (req, res) => {
+  //code here for POST
+});
+
+router.route("/persondetails").get(async (req, res) => {
+  //code here for GET
+});
